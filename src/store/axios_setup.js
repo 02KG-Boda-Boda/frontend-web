dc851c9b577eeb2726a98d978c38a6132a7c0056
wamozo cosmas <wcosmas75@gmail.com>
2020-07-16 00:58:18 +0300

    Edited env files

--- src/store/axios_setup.js
+++ src/store/axios_setup.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:3000/api/V1";
+const API_URL = "http://178.128.100.246:8080/api/V1";
 const token = localStorage.getItem("token");
 
 let settings = {
